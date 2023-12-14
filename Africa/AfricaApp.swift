f61d9cce9efb0c269886db403b3f288fe9fb5d10
Mohamed Ramadan <mohamed.moslem50@gmail.com>
2023-12-14 11:22:34 +0200

    Add existing project files prior to the push to GitHub.

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by mohamed ramadan on 14/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
